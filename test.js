b242ced32f28f88c37de6ff61f644559aa6c65c3
Sergiu Uifalean <sergiu.uifalean@tss-yonder.com>
2019-05-24 12:46:21 +0300

    stripped down the unnecessary stuff

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
-const { remote } = require('webdriverio');
+const { multiremote } = require('webdriverio');
 
+let browser = null;
 (async () => {
-    const browser = await remote({
-        logLevel: 'error',
-        path: '/',
-        capabilities: {
-            browserName: 'firefox'
+    browser = await multiremote({
+        myChromeBrowser: {
+            capabilities: {
+                browserName: 'chrome'
+            }
+        },
+        myFirefoxBrowser: {
+            capabilities: {
+                browserName: 'firefox'
+            }
         }
     });
 
@@ -14,3 +20,22 @@ const { remote } = require('webdriverio');
     const title = await browser.getTitle();
     console.log('Title was: ' + title);
 })().catch((e) => console.error(e));
+
+const http = require('http');
+
+
+
+// Create an instance of the http server to handle HTTP requests
+let app = http.createServer((req, res) => {  
+    // Set a response type of plain text for the response
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    browser.url('http://www.amazon.com');
+
+    // Send back a response and end the connection
+    res.end('Hello World!\n');
+});
+
+// Start the server on port 3000
+app.listen(3000, '127.0.0.1');  
+console.log('Node server running on port 3000');  
