54bf959c4639e9d2d4d14aa0641de855443210aa
Peter Sandor <peter.sandor@tss-yonder.com>
2019-05-25 10:34:06 +0300

    remove external JS files, merge client scripts and bootstrap master (chrome) browser and inject scripts into it

--- coordinator-server/test.js
+++ coordinator-server/test.js
@@ -1,10 +1,23 @@
 const { multiremote } = require('webdriverio');
 const EventDispatcher = require('./event-framework/event-dispatcher');
+
+// TODO this must come from config/CLI argument
+let appUrl = 'https://clarity.design';
+
+let master = null;
 let browser = null;
 let eventDispatcher = null;
 (async () => {
-    browser = await multiremote({
+    master = await multiremote({
         myChromeBrowser: {
+            capabilities: {
+                browserName: 'chrome'
+            }
+        }
+    });
+
+    browser = await multiremote({
+        myEdgeBrowser: {
             capabilities: {
                 browserName: 'MicrosoftEdge'
             }
@@ -17,12 +30,40 @@ let eventDispatcher = null;
     });
     eventDispatcher = new EventDispatcher(browser);
 
+    await browser.url(appUrl);
 
-    await browser.url('http://192.168.56.1:3000/index');
+    // start master and inject necessary scripts into it
+    await master.url(appUrl);
+    master.execute(injectScripts);
 
     const title = await browser.getTitle();
     console.log('Title was: ' + title);
 })().catch((e) => console.error(e));
+
+// TODO make this code look better
+// TODO this works for SPAs only, need a tool to inject scripts for MPAs. tampermonkey works (chrome extension)
+injectScripts = function() {
+    injectScript = (source, callback) => {
+        var script = document.createElement('script');
+        script.async = 1;
+        script.src = source;
+    
+        script.onload = script.onreadystatechange = (_, isAbort) => {
+            if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+                script.onload = script.onreadystatechange = null;
+                script = undefined;
+                if (!isAbort && callback) callback();
+            }
+        };
+        
+        document.head.appendChild(script);
+    };
+
+    injectScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+        () => injectScript('https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js',
+            () => injectScript('https://cdnjs.cloudflare.com/ajax/libs/optimal-select/4.0.1/optimal-select.js',
+                () => injectScript('http://localhost:3000/client.js', () => console.log('all scripts loaded')))));
+}
 
 var app = require('express')();
 const http = require('http').Server(app);
@@ -39,10 +80,10 @@ app.get('/client.js', function(req, res){
 io.on('connection', function(socket){
     console.log('');
     socket.on('clientEvent', function (evt) {
-        eventDispatcher.dispatch('click', evt);
+        eventDispatcher.dispatch(evt.name, { selector: evt.identifier });
     })
 });
 
 // Start the server on port 3000
-http.listen(3000, '192.168.56.1');  
+http.listen(3000, 'localhost');  
 console.log('Node server running on port 3000');  
