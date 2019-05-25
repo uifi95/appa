227deb67b48ebe03ddbbbe89b2e21e33567ed64c
Sergiu Uifalean <sergiu.uifalean@tss-yonder.com>
2019-05-25 13:43:24 +0300

    update deployment, add readme, and update code for inject-script

--- coordinator-server/test.js
+++ coordinator-server/test.js
@@ -1,6 +1,6 @@
-const { multiremote } = require('webdriverio');
+const { multiremote, remote } = require('webdriverio');
 const EventDispatcher = require('./event-framework/event-dispatcher');
-
+const injectScripts = require('./lib/inject-scripts');
 // TODO this must come from config/CLI argument
 let appUrl = 'https://clarity.design';
 
@@ -8,11 +8,9 @@ let master = null;
 let browser = null;
 let eventDispatcher = null;
 (async () => {
-    master = await multiremote({
-        myChromeBrowser: {
-            capabilities: {
-                browserName: 'chrome'
-            }
+    master = await remote({
+        capabilities: {
+            browserName: 'chrome'
         }
     });
 
@@ -39,31 +37,6 @@ let eventDispatcher = null;
     const title = await browser.getTitle();
     console.log('Title was: ' + title);
 })().catch((e) => console.error(e));
-
-// TODO make this code look better
-// TODO this works for SPAs only, need a tool to inject scripts for MPAs. tampermonkey works (chrome extension)
-injectScripts = function() {
-    injectScript = (source, callback) => {
-        var script = document.createElement('script');
-        script.async = 1;
-        script.src = source;
-    
-        script.onload = script.onreadystatechange = (_, isAbort) => {
-            if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
-                script.onload = script.onreadystatechange = null;
-                script = undefined;
-                if (!isAbort && callback) callback();
-            }
-        };
-        
-        document.head.appendChild(script);
-    };
-
-    injectScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js',
-        () => injectScript('https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js',
-            () => injectScript('https://cdnjs.cloudflare.com/ajax/libs/optimal-select/4.0.1/optimal-select.js',
-                () => injectScript('http://localhost:3000/client.js', () => console.log('all scripts loaded')))));
-}
 
 var app = require('express')();
 const http = require('http').Server(app);
