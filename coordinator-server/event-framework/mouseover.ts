28ab7cabcc6eda46902e7f5342b06df394ea387f
Peter Sandor <peter.sandor@tss-yonder.com>
2020-10-17 15:48:51 +0300

    use typed browser in all event handlers

--- coordinator-server/event-framework/mouseover.ts
+++ coordinator-server/event-framework/mouseover.ts
@@ -1,3 +1,4 @@
+import { Browser } from "webdriverio";
 import { Event } from "./event";
 
 export class MouseOver extends Event {
@@ -5,11 +6,11 @@ export class MouseOver extends Event {
         super(eventDescriptor);
     }
 
-    async trigger(browser) {
+    async trigger(browser: Browser) {
         const driverElement = await super.getElement(browser);
         const { size: masterSize, position, identifier } = this.eventDescriptor;
 
-        const [slaveSize] = await browser.execute(
+        const slaveSize = (await browser.execute(
             function mouseOver(identifier) {
                 const element = document.querySelector(identifier);
                 const rectangle = element.getBoundingClientRect();
@@ -19,7 +20,7 @@ export class MouseOver extends Event {
                 };
             },
             identifier
-        );
+        ))[0];
 
         const x = position.x * masterSize.width / slaveSize.width;
         const y = position.y * masterSize.height / slaveSize.height;
